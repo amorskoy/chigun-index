e566c66e76fdddbaf778a1775a94f84d73531c7d
Andrey Morskoy <mora@ciklum.com>
2017-04-21 16:12:04 +0300

    First draft commit

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "Chigun indexer"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.0"
+libraryDependencies += "org.elasticsearch" %% "elasticsearch-spark-20_2.10" % "5.3.0"
+
+
---
+++ src/scala/chigun/SaveToES.scala
@@ -0,0 +1,35 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object SaveToES {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("SaveToES")
+    conf.set("es.index.auto.create", "true")
+    conf.set("es.net.http.auth.user", "elastic")
+    conf.set("es.net.http.auth.pass", "changeme")
+    
+    val sc = new SparkContext(conf)
+
+    val base = "/projects/chigun-index/resources/txt/"
+    val f = "1991_1.htm.txt"
+    val fp = base + f
+    val textFile = sc.textFile(fp)
+
+    // OR
+
+    val files = sc.wholeTextFiles("/projects/chigun-index/resources/txt/*")
+
+
+    textFile
+	.flatMap(line => line.split(" "))
+	.filter(_.length>3)
+	.map(_.toLowerCase)
+	.map(word => (word, 1))
+	.reduceByKey((a, b) => a + b)
+	.sortBy(_._2)
+    .collect().foreach(println)
+
+    sc.stop()
+  }
+}
